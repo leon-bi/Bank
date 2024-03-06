63b93079b1d0842728babc142a79ae9cffa806bd
leon-bi <leonbichanga@gmail.com>
2024-03-06 20:42:21 +0300

    add gitignore

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
-const express = require('');
-const app = express;
+const express = require('express');
+//initialise the app
+const app = express();
+
+app.listen(3000,() => {
+    console.log('server is running on port 3000')
+});
+
+app.get('/',(req, res) =>{
+ res.send('Hello from Node API');
+})
